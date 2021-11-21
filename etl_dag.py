b2f484abb5f5cb8a4b531d45861724f94d868509
Sérgio Passos <sergio.passos88@gmail.com>
2021-11-21 16:10:25 -0300

    Update etl_dag.py

--- etl_dag.py
+++ etl_dag.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 from airflow.contrib.operators import kubernetes_pod_operator
 
 default_args = {
-    'owner': 'Damavis',
+    'owner': 'Sergio',
     'start_date': datetime(2020, 5, 5),
     'retries': 1,
     'retry_delay': timedelta(seconds=5)
@@ -14,7 +14,7 @@ with DAG('etl_dag',
          schedule_interval=None) as dag:
 
     extract_tranform = kubernetes_pod_operator.KubernetesPodOperator(
-        namespace='airflow',
+        namespace='default',
         image="python:3.7-slim",
         cmds=["echo"],
         arguments=["This can be the extract part of an ETL"],
