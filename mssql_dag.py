80e305c7502cb2bc6bb98b8d6659987b4fc03ad9
Sérgio Passos <sergio.passos88@gmail.com>
2021-11-22 22:25:50 -0300

    Update mssql_dag.py

--- mssql_dag.py
+++ mssql_dag.py
@@ -9,6 +9,10 @@ default_args = {
     'retry_delay': timedelta(seconds=5)
 }
 
+BASE_FOLDER = "datalake/{stage}/{partition}"
+
+PARTITION_FOLDER = "extract_date={{ ds }}"
+
 with DAG('mssql_dag',
          default_args=default_args,
          schedule_interval=None) as dag:
@@ -29,9 +33,9 @@ with DAG('mssql_dag',
              name="basic",
              application_args=[
                  "--src",
-                 BASE_FOLDER.format(stage="bronze", partition=PARTITION_FOLDER),
+                 BASE_FOLDER.format(stage="raw", partition=PARTITION_FOLDER),
                  "--dest",
-                 BASE_FOLDER.format(stage="silver", partition=""),
+                 BASE_FOLDER.format(stage="cleansed", partition=""),
                  "--process-date",
                  "{{ ds }}",
             ]
