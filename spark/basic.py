f27261f56f2db9f021d224a5a21749977b207b48
Sérgio Passos <sergio.passos88@gmail.com>
2021-11-23 17:05:35 -0300

    Update basic.py

--- spark/basic.py
+++ spark/basic.py
@@ -3,10 +3,8 @@ from os.path import join
 
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as f
+from spark.utils.base_functions import get_data
 
-
-def get_data(df):
-    return df.select()
 
 def export_json(df, dest):
     df.coalesce(1).write.mode("overwrite").json(dest)
@@ -19,3 +17,19 @@ def data_transform(spark, src, dest, process_date):
     table_dest = join(dest, "{table_name}", f"process_date={process_date}")
 
     export_json(data_df, table_dest.format(table_name="test_data"))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Spark Twitter Transformation"
+    )
+    parser.add_argument("--src", required=True)
+    parser.add_argument("--dest", required=True)
+    parser.add_argument("--process-date", required=True)
+    args = parser.parse_args()
+
+    spark = SparkSession\
+        .builder\
+        .appName("basic")\
+        .getOrCreate()
+
+    data_transform(spark, args.src, args.dest, args.process_date)
